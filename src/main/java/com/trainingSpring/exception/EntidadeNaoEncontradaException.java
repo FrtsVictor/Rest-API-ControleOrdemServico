b2e4575da9d8a908b30ed55c576cc8d5369f2f47
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-24 23:40:33 -0300

    Add EntidadeNaoEncontradaException /exception

---
+++ src/main/java/com/trainingSpring/exception/EntidadeNaoEncontradaException.java
@@ -0,0 +1,13 @@
+package com.trainingSpring.exception;
+
+public class EntidadeNaoEncontradaException extends NegocioException {
+
+	private static final long serialVersionUID = 4945636341203248863L;
+
+	public EntidadeNaoEncontradaException(String message) {
+		super(message);
+		
+	}
+
+	
+}
