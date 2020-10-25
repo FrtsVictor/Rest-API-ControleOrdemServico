93991b0fb6f6b8a2b3d34eff289b9e81f8f43ee9
FrtsVictor <victorfreitas.job@gmail.com>
2020-10-24 23:38:39 -0300

    Add ComentarioRepository  /repository

---
+++ src/main/java/com/trainingSpring/repository/ComentarioRepository.java
@@ -0,0 +1,9 @@
+package com.trainingSpring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.trainingSpring.model.Comentario;
+
+public interface ComentarioRepository extends JpaRepository<Comentario, Long>{
+
+}
